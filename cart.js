62d13c51c16669a6dc1a2f11c5aa2e3e01529d34
Jean-Eudes WOTO <107761073+NF31@users.noreply.github.com>
2022-08-17 18:30:31 +0200

    Add files via upload

---
+++ cart.js
@@ -0,0 +1,12 @@
+
+
+//ajout au panier via le bouton d'ajout
+
+let button  = document.getElementById("addCart") 
+
+
+button.addEventListener("click",() => {
+    //je souhaite accéder au localstorage pour vérifier si un panier existe 
+    //si il existe je le récupère rajoute le produit en question puis remet le nouveau panier ds le LS
+    //sinon je le créer et je met le produit en question
+})
